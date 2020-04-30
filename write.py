bfdd47362500d30c6eea4a26fab0d5af3a7d46b3
edward <edward.yu0830@gmail.com>
2020-04-30 23:21:21 +0800

    now with test words, and can handle exception when info.txt not found

--- write.py
+++ write.py
@@ -1,8 +1,22 @@
 import pyautogui as pgui
 import time
 import pyperclip as pc
+run = 2
+duration = 0.08
+try:
+        f = open("info.txt","r",encoding="utf-8")
+except FileNotFoundError:
+        print("please place info.txt in ./")
+        exit(1)
+print("Wait 5 sec to start...")
 time.sleep(5)
-for i in range(10):
-    pc.copy('💖')
-    pgui.hotkey('ctrl','v')
-    time.sleep(0.1)
+infoList = f.readlines()
+for i in range(run):
+        for line in infoList:
+                line = line[:-1]
+                pc.copy(line)
+                pgui.hotkey('ctrl','v')
+                pgui.press('enter')
+                time.sleep(duration)
+print("End script...")
+f.close()
